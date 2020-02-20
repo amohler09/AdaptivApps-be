f30dd1346aef107e5ebd06b0b4295a315d0d2f9c
JonathanChen <jonny29009@yahoo.com>
2020-02-20 12:03:47 -0800

    create Mutation for profile

--- apollo/src/resolvers/Mutation.js
+++ apollo/src/resolvers/Mutation.js
@@ -13,6 +13,34 @@ const createProfile = (_, args, context) => {
   return profile;
 };
 
+/**
+ * @param {{ data: import('../generated/prisma-client').ProfileUpdateInput, where: import('../generated/prisma-client').ProfileWhereUniqueInput }} args
+ * @param {{ prisma: import('../generated/prisma-client').Prisma }} context
+ * @returns { import('../generated/prisma-client').ProfilePromise }
+ */
+const updateProfile = (_, args, context) => {
+  console.log("updateProfile.args: %j", args)
+
+  const profile = context.prisma.updateProfile(args);  
+
+  return profile;
+};
+
+/**
+ * @param {{ where: import('../generated/prisma-client').ProfileWhereUniqueInput }} args
+ * @param {{ prisma: import('../generated/prisma-client').Prisma }} context
+ * @returns { import('../generated/prisma-client').ProfilePromise }
+ */
+const deleteProfile = (_, args, context) => {
+  console.log("deleteProfile.args: %j", args);
+
+  const profile = context.prisma.deleteProfile(args.where);
+
+  return profile;
+};
+
 module.exports = {
   createProfile,
+  updateProfile,
+  deleteProfile
 };
