1a98eaaff289eeeec2dbc27278950a6a2f5cfa59
Robert Campbell <Campbell.Rob@protonmail.com>
2020-02-23 09:59:23 -0800

    added new deps, imports index.js

--- apollo/src/index.js
+++ apollo/src/index.js
@@ -1,32 +1,33 @@
 // @ts-check
 
-const { importSchema } = require("graphql-import");
+const { importSchema } = require('graphql-import');
+const { ApolloServer, gql, AuthenticationError } = require('apollo-server');
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
+
+const resolvers = require('./resolvers');
+const context = require('./context');
 
 const PORT = process.env.PORT || 8000;
-const { ApolloServer, gql } = require("apollo-server");
-
-const resolvers = require("./resolvers");
-const context = require("./context");
-
 
 async function main() {
-  console.log("Importing schema")
+  console.log('Importing schema');
 
-  const typeDefs = await importSchema("schema/schema.graphql")
+  const typeDefs = await importSchema('schema/schema.graphql');
 
-  console.log(typeDefs)
+  console.log(typeDefs);
 
-  console.log("Imported schema")
+  console.log('Imported schema');
 
   const server = new ApolloServer({
     resolvers,
     typeDefs: gql(typeDefs),
     context,
     cors: true,
-    dataSources: () => ({})
+    dataSources: () => ({}),
   });
-  
-  server.listen({port: PORT}).then(({ url }) => {
+
+  server.listen({ port: PORT }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
   });
 }
