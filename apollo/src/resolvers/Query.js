d96b7e8dbed4f760bc2924c19e1cd571d473dcda
JonathanChen <jonny29009@yahoo.com>
2020-02-25 13:36:11 -0800

    add comment and enforce authorization in mutation resolvers

--- apollo/src/resolvers/Query.js
+++ apollo/src/resolvers/Query.js
@@ -9,12 +9,10 @@
  * @returns { Promise }
  */
 const profile = async (_, args, context) => {
-  console.log('Query.profile.args: %j', args);
-
+  // Finding the profile based on args specification
   const profile = await context.prisma.profile(args.where);
+  // This next line ensures user needs to be logged in, else return error
   const user = await context.user;
-  console.log('The glorious user is', user);
-  console.log('Query.profile: %j', profile);
 
   return profile;
 };
@@ -25,12 +23,11 @@ const profile = async (_, args, context) => {
  * @returns { Promise }
  */
 const profiles = async (_, args, context) => {
-  console.log('Query.profiles.args should be empty: %j', args);
-
+  // Returns all profiles
   const profile = await context.prisma.profiles(args);
+  // This next line ensures user needs to be logged in, else return error
   const user = await context.user;
-  console.log('Query.profiles: %j', profile);
-  console.log('The user is', user);
+
   return profile;
 };
 
