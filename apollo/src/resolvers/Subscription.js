30e581be207d35869e7467992d00f896e22e81cd
JonathanChen <jonny29009@yahoo.com>
2020-03-04 10:04:41 -0800

    working in mid of subscription

---
+++ apollo/src/resolvers/Subscription.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+/**
+ * @param {{ where: import('../generated/prisma-client').ProfileSubscriptionWhereInput }} args
+ * @param {{ prisma: import('../generated/prisma-client').Prisma }} context
+ * @returns { Promise }
+ */
+const profile = async (_, args, context) => {
+  // This next line ensures user needs to be logged in, else return error
+  const user = await context.user;
+  console.log('Hitting here', user);
+  const subscribed = await context.prisma.$subscribe
+    .profile({ mutation_in: ['CREATED'] })
+    .node();
+  return subscribed;
+};
+
+module.exports = {
+  profile,
+};
--- apollo/src/resolvers/index.js
+++ apollo/src/resolvers/index.js
@@ -7,10 +7,12 @@
 
 const Query = require('./Query');
 const Mutation = require('./Mutation');
+const Subscription = require('./Subscription');
 
 const resolvers = {
   Query,
-  Mutation
+  Mutation,
+  Subscription,
 };
 
 module.exports = resolvers;
