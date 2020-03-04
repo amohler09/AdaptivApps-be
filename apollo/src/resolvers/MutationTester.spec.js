89a3d1c3e5dadfd7bb285cff2344bd90e5a4111b
JonathanChen <jonny29009@yahoo.com>
2020-03-04 09:14:58 -0800

    saving tests

---
+++ apollo/src/resolvers/MutationTester.spec.js
@@ -0,0 +1,7 @@
+const profileCreateInput = require('../generated/prisma-client');
+const { createProfile } = require('./Mutation.js');
+
+it('Calls prisma', async () => {
+  const fakePrismaCreateProfile  
+  await createProfile();
+});
