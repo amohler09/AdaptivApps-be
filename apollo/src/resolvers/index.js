b56fadbb714f0a8f4f439c06513dd61fd5cc6792
Priya <pvaidya56@gmail.com>
2020-05-08 14:37:00 -0700

    Announcement subscriptions

--- apollo/src/resolvers/index.js
+++ apollo/src/resolvers/index.js
@@ -14,6 +14,7 @@ const Participant = require('./Participant');
 const Chat = require('./Chat');
 const ChatRoom = require('./ChatRoom');
 const Subscription = require('./Subscription');
+const Announcement = require('./Announcement');
 
 const resolvers = {
   Query,
@@ -24,7 +25,8 @@ const resolvers = {
   Event,
   Participant,
   Chat,
-  ChatRoom
+  ChatRoom,
+  Announcement
 };
 
 module.exports = resolvers;
