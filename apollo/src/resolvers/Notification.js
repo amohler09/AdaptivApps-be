d840a357cda9460435581e82b8545f2add95b614
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-05-19 16:43:25 -0400

    Import notifications to resolvers to return connected tables

--- apollo/src/resolvers/Notification.js
+++ apollo/src/resolvers/Notification.js
@@ -9,6 +9,7 @@ function announcement (parent, __, context) {
 function chat (parent, __, context) {
   return context.prisma.notification({ id: parent.id }).chat()
 }
+
 module.exports = {
   profile,
   announcement,
